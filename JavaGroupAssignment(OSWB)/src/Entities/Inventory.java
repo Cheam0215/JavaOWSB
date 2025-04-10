a00d807e390c6a461db017cdbd4178074b5fd04d
CMX0609 <maxcmx480@gmail.com>
2025-04-11 01:29:50 +0800

    Update Inventory.java

--- JavaGroupAssignment(OSWB)/src/Entities/Inventory.java
+++ JavaGroupAssignment(OSWB)/src/Entities/Inventory.java
@@ -6,15 +6,24 @@ package Entities;
 
 import java.util.List;
 
+import Utility.FileManager;
+
 /**
  *
  * @author Sheng Ting
  */
 public class Inventory {
     private static final int LOW_STOCK_THRESHOLD = 100; // Low stock threshold
-    
-    public List<Item> viewItems () {
-        return null;
+    private FileManager fileManager = new FileManager();
+
+    public List<Item> viewItems() {
+        return fileManager.readFile(
+            fileManager.getItemFilePath(),
+            line -> {
+                String[] data = line.split(",");
+                return new Item(data[0], data[1], data[2], Integer.parseInt(data[3]));
+            }
+        );
     }
     
     public boolean updateStock() {
