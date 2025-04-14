7a5911e5d943ae49dccf6d282451a92221cd1a2f
clamusiclover <chonglian23@gmail.com>
2025-04-15 00:15:43 +0800

    Purchase manager View functions

--- src/Entities/PurchaseManager.java
+++ src/Entities/PurchaseManager.java
@@ -1,5 +1,8 @@
 package Entities;
 
+import Utility.FileManager;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,25 +13,101 @@ package Entities;
  * @author Sheng Ting
  */
 public class PurchaseManager extends User{
+    private final FileManager fileManager;
     
     public PurchaseManager(String userId, String username, String password) {
         super(userId, username, password, "PURCHASE_MANAGER");
+        this.fileManager = new FileManager();
     }
     
     public String viewItems() {
-        return null;
+        List<Item> itemsList = fileManager.readFile(
+            fileManager.getPoFilePath(),
+            line -> {
+                String[] data = line.split(",");
+                return new Item(data[0], data[1], data[2], 
+                    Integer.parseInt(data[3]));
+            }
+        );
+
+        StringBuilder view = new StringBuilder("Item List:\n");
+        view.append("Item Code|Item Name|Supplier ID|Stock Level\n");
+        view.append("-------------------------------------------\n");
+
+        for (Item item : itemsList) {
+            view.append(String.format("%s | %s | %s | %d \n",
+                item.getItemCode(), item.getItemName(), item.getSupplierId(), 
+                item.getStockLevel()));
+        }
+        return view.toString();
     }
     
     public String viewSuppliers() {
-        return null;
+        List<Supplier> supplierList = fileManager.readFile(
+            fileManager.getPoFilePath(),
+            line -> {
+                String[] data = line.split(",");
+                return new Supplier(data[0], data[1]);
+            }//2 is list may got problem
+        );
+
+        StringBuilder view = new StringBuilder("Item List:\n");
+        view.append("Supplier Code|Supplier Name\n");
+        view.append("-------------------------------------------\n");
+
+        for (Supplier sup : supplierList) {
+            view.append(String.format("%s | %s \n",
+                sup.getSupplierCode(), sup.getSupplierName(), sup.getItemIds()));
+        }
+        return view.toString();
     }
     
     public String viewPurchaseRequisition() {
-        return null;
+        List<PurchaseRequisition> prList = fileManager.readFile(
+            fileManager.getPrFilePath(),
+            line -> {
+                String[] data = line.split(",");
+                return new PurchaseRequisition(data[0], data[1], 
+                    Integer.parseInt(data[2]), data[3], data[4]);
+            }
+        );
+
+        StringBuilder view = new StringBuilder("Purchase Requisitions:\n");
+        view.append("PR ID | Item Code | Quantity | Required Date | Supplier\n");
+        view.append("-------------------------------------------------\n");
+
+        for (PurchaseRequisition pr : prList) {
+            view.append(String.format("%s | %s | %d | %s | %s\n",
+                pr.getPrId(), pr.getItemCode(), pr.getQuantity(),
+                pr.getRequiredDate(), pr.getSupplierCode()));
+        }
+        return view.toString();
+    }
+    
+    public String viewPurchaseOrder() {
+         List<PurchaseOrder> poList = fileManager.readFile(
+            fileManager.getPoFilePath(),
+            line -> {
+                String[] data = line.split(",");
+                return new PurchaseOrder(data[0], data[1], data[2], 
+                    Integer.parseInt(data[3]), data[4], data[5], Double.parseDouble(data[6]));
+            }
+        );
+         
+        StringBuilder view = new StringBuilder("Purchase Order:\n");
+        view.append("Purchase Order ID | Purchase Requisition ID | Item Code | Quantity | Supplier ID | Status | Payment Amount\n");
+        view.append("-------------------------------------------------\n");
+
+        for (PurchaseOrder po : poList){
+            view.append(String.format("%s | %s | %s | %d | %s | %s | RM%.2f\n",
+                po.getPoId(), po.getPrId(), po.getItemCode(),
+                po.getQuantity(), po.getSupplierCode(),po.getStatus(), po.getPaymentAmount()));   
+        }
+        return view.toString();
     }
     
     public String generatePurchaseOrder() {
-        return null;
+        return "";
     }
     
     public boolean editPurchaseOrder() {
@@ -38,9 +117,7 @@ public class PurchaseManager extends User{
     public boolean deletePurchaseOrder() {
         return true;
     }
-    public String viewPurchaseOrder() {
-        return null;
-    }
+    
     
     
     
