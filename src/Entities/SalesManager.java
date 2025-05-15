9626b8317834f032db6bc54cfbdf7a2da7ae7220
Edwinchen04 <edwinchen04@gmail.com>
2025-05-15 22:56:18 +0800

    touch up

--- src/Entities/SalesManager.java
+++ src/Entities/SalesManager.java
@@ -12,7 +12,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
