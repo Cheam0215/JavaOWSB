67e35c918caf57b4eea5de9d7c9a817edff93476
Cheam Sheng Ting <deciatto.shengting@gmail.com>
2025-05-19 00:13:34 +0800

    Fix on IM
    Copied Eten's file over

--- src/Entities/InventoryManager.java
+++ src/Entities/InventoryManager.java
@@ -24,12 +24,10 @@ import javax.swing.table.DefaultTableModel;
  */
 public class InventoryManager extends User{
     
-//    private final Inventory inventory;
     private final FileManager fileManager;
 
     public InventoryManager(String userId, String username, String password) {
         super(userId, username, password, UserRoles.INVENTORY_MANAGER);
-//        this.inventory = new Inventory();
         this.fileManager = new FileManager();
     }
     
@@ -255,17 +253,10 @@ public class InventoryManager extends User{
     
     }
     
-//    public void trackLowStock() {
-//        inventory.checkLowStock();
-//    }
-//    
-    public String viewPurchaseOrder(){
-        return null;
-    }
     
-    public void displayStockReport() {
-        LocalDate today = LocalDate.now();
-        String manager = getUsername();
+    public void displayStockReport(String name, String startDate, String endDate) {
+        String manager = name;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
         System.out.println("Generating report for range: " + startDate + " to " + endDate);
 
